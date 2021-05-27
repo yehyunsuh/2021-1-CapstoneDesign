2ee3b034368efc710e3731c7740121eab2f9955e
Jungjjeong <sky990106@dgu.ac.kr>
2021-05-27 17:44:46 +0900

    Distance
    0527

--- JiYoung/sceneform-android-sdk-master/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/DistanceActivity.java
+++ JiYoung/sceneform-android-sdk-master/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/DistanceActivity.java
@@ -1,13 +1,150 @@
 package com.google.ar.sceneform.samples.gltf;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class DistanceActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Pose;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.Objects;
+
+public class DistanceActivity extends AppCompatActivity implements Scene.OnUpdateListener {
+
+
+    private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ArFragment arFragment;
+    private Anchor[] currentAnchor = new Anchor[2];
+    private AnchorNode[] currentAnchorNode = new AnchorNode[2];
+    private TextView tvDistance;
+    ModelRenderable cubeRenderable;
+    public static int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            Toast.makeText(getApplicationContext(), "Device not supported", Toast.LENGTH_LONG).show();
+        }
+
         setContentView(R.layout.activity_distance);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        tvDistance = findViewById(R.id.tvDistance);
+
+
+        initModel();
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            if (cubeRenderable == null)
+                return;
+
+
+            // Creating Anchor.
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+            clearAnchor();
+
+            currentAnchor[cnt] = anchor;
+            currentAnchorNode[cnt] = anchorNode;
+
+            cnt++;
+
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setRenderable(cubeRenderable);
+            node.setParent(anchorNode);
+            arFragment.getArSceneView().getScene().addOnUpdateListener(this);
+            arFragment.getArSceneView().getScene().addChild(anchorNode);
+            node.select();
+
+
+        });
+
+    }
+
+    public boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+
+        String openGlVersionString =
+                ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+
+    private void initModel() {
+        MaterialFactory.makeTransparentWithColor(this, new Color(android.graphics.Color.RED))
+                .thenAccept(
+                        material -> {
+                            Vector3 vector3 = new Vector3(0.05f, 0.01f, 0.01f);
+                            cubeRenderable = ShapeFactory.makeCube(vector3, Vector3.zero(), material);
+                            cubeRenderable.setShadowCaster(false);
+                            cubeRenderable.setShadowReceiver(false);
+                        });
+    }
+
+    private void clearAnchor() {
+
+        if(cnt == 2){
+            for(int i=0; i<2; i++){
+                currentAnchor[i] = null;
+                if (currentAnchorNode[i] != null) {
+                    arFragment.getArSceneView().getScene().removeChild(currentAnchorNode[i]);
+                    currentAnchorNode[i].getAnchor().detach();
+                    currentAnchorNode[i].setParent(null);
+                    currentAnchorNode = null;
+                }
+            }
+            cnt = 0;
+        }
+    }
+
+
+    @Override
+    public void onUpdate(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+
+        if (currentAnchorNode != null && cnt == 2) {
+            Pose objectPose = currentAnchor[0].getPose();
+            Pose newPose = currentAnchor[1].getPose();
+
+            float dx = objectPose.tx() - newPose.tx();
+            float dy = objectPose.ty() - newPose.ty();
+            float dz = objectPose.tz() - newPose.tz();
+
+            ///Compute the straight-line distance.
+            float distanceMeters = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+            tvDistance.setText("Distance: " + distanceMeters*100 + " centimeters");
+
+        }
     }
 }
